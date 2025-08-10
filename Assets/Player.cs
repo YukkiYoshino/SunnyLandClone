39fcdf2256f8bcc03d9884c9f305d5b7bee67467
YukkiYoshino <60950986+YukkiYoshino@users.noreply.github.com>
2025-08-10 16:02:25 -0300

    AttMapaTi
    Uso de Tiles Rules

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,16 +1,39 @@
+﻿using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float forcaMovimento = 1f;
+    public float forcaPulo = 7f;
+    public float velocidadeMaxima = 5f;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float direcao = Input.GetAxisRaw("Horizontal");
+
+        // Aplica impulso horizontal se ainda não estiver na velocidade máxima
+        if (Mathf.Abs(rb.velocity.x) < velocidadeMaxima)
+        {
+            rb.AddForce(Vector2.right * direcao * forcaMovimento, ForceMode2D.Impulse);
+        }
+
+        // Pulo com impulso ao pressionar espaço
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+        }
+
+        // Virar o personagem para a direção do movimento
+        if (direcao != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(direcao), 1, 1);
+        }
     }
 }
