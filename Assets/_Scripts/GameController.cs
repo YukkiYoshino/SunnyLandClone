ae0284a9ff3e6c40afbe2923bae4373f16e268c8
YukkiYoshino <60950986+YukkiYoshino@users.noreply.github.com>
2025-08-26 01:29:27 -0300

    att menu

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,5 +1,7 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class GameController : MonoBehaviour
@@ -11,5 +13,48 @@ public class GameController : MonoBehaviour
     {
         pontos += qtdPontos;
         txtPontos.text = pontos.ToString();
+    }
+
+    public void CarregarCenaPorNome(string nomeCena)
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+
+    public void FecharJogo()
+    {
+        Application.Quit();
+    }
+
+
+    public GameObject painelPause; 
+    private bool pausado = false;
+
+    void Start()
+    {
+        painelPause.SetActive(false); // começa desligado
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PausarOuDespausar();
+        }
+    }
+
+    public void PausarOuDespausar()
+    {
+        pausado = !pausado;
+
+        if (pausado)
+        {
+            Time.timeScale = 0f; // congela o jogo
+            painelPause.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f; // volta o tempo ao normal
+            painelPause.SetActive(false);
+        }
     }
 }
